5bfed5e572c69e1ac1e57a1f585f95b9dc9c979d
joao victor <jvsampaiio15@gmail.com>
2026-05-06 13:36:13 -0300

    correção de erro

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,5 @@
 
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -78,19 +79,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'postgres'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-        'OPTIONS': {
-            'sslmode': os.getenv('DB_SSLMODE', 'require'),
-        },
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL', 'postgres://postgres:postgres@localhost:5432/postgres'),
+        conn_max_age=600
+    )
 }
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
